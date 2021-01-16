34f48cd1bc6251eaca6e26bf21cdcf92a897449d
Ramdane Lagha <ramdane.lagha@ansys.com>
2021-01-16 11:36:09 +0100

    add examples

--- ansys/dpf/core/examples/downloads.py
+++ ansys/dpf/core/examples/downloads.py
@@ -125,3 +125,26 @@ def download_all_kinds_of_complexity_modal() -> str:
 
     """
     return _download_file('testing', 'modal_allKindOfComplexity.rst')
+
+def download_multi_harmonic_result() -> str:
+    """Download an example harmonic result file and return the download path.
+
+    Examples files are downloaded to a persistent cache to avoid
+    re-downloading the same file twice.
+
+    Returns
+    -------
+    str
+        Path to the example file.
+
+    Examples
+    --------
+    Download an example result file and return the path of the file
+
+    >>> from ansys.dpf.core import examples
+    >>> path = examples.transient_result
+    >>> path
+    'C:/Users/user/AppData/local/temp/multi_harmonic.rst'
+
+    """
+    return _download_file('testing', 'file_harmonic_5rpm.rst')
