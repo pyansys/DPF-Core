f66742b9f93392cfb6541156f9832fe2ac954017
Ramdane Lagha <ramdane.lagha@ansys.com>
2021-01-17 15:17:20 +0100

    add examples

--- examples/02-modal-harmonic/00-multi_harmonic.py
+++ examples/02-modal-harmonic/00-multi_harmonic.py
@@ -1,83 +1,92 @@
 """
-.. _ref_multi_harmonic_example:
+.. _ref_basic_harmonic:
 
-Multi harmonic result example
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This example shows how to compute multi harmonic responses.
-
-
-If you have ANSYS 2021R1 installed, getting running DPF is quite easy
-as `ansys.dpf.core` takes care of launching all the services required
-to start post-processing ANSYS files.
-
-First, import the DPF-Core module as `dpf_core` and also import the
-included examples file.
-
+Multi-Harmonic Response Example
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This example shows how to to compute a multi-harmonic response 
+using fft transforms
 
 """
+import numpy as np
+import matplotlib.pyplot as pyplot
 
 from ansys.dpf import core as dpf
 from ansys.dpf.core import examples
 
 ###############################################################################
-# Next, open an example and then print out the ``model`` object.  The
-# ``Model`` class helps to organize access methods for the result by
-# keeping track of the operators and data sources used by the result
-# file.
+# Begin by downloading the example harmonic result.  This result is
+# not included in the core module by default to speed up the install.
+# Download should only take a few seconds.
 #
-# Printing the model displays:
-# - Analysis type
-# - Available results
-# - Size of the mesh
-# - Number of results
-#
-# Also, note that the first time you create a DPF object, Python will
-# automatically attempt to start the server in the background.  If you
-# wish to connect to an existing server (either local or remote), use
-# ``dpf.connect_to_server()``
+# Next, create the model and display the state of the result.  Note
+# that this harmonic result file contains several rpms,
+# each rpm has several frequencies.
 
-model = dpf.Model(examples.simple_bar)
+harmonic = r'c:\temp\file_harmonic_5rpm.rst'
+model = dpf.Model(harmonic)
 print(model)
 
 
 ###############################################################################
-# Model Metadata
-# ~~~~~~~~~~~~~~
-# Specific metadata can be extracted from the model by referencing the
-# ``metadata`` property of the model.  For example, just the
-# result_info can be printed with:
-
-metadata = model.metadata
-print(metadata.result_info)
-
+# Read the analysis domain support 
+tf = model.metadata.time_freq_support
+#print(model.metadata.time_freq_support)
 
 ###############################################################################
-# The mesh region can be printed with
-
-print(metadata.meshed_region)
-
-
-###############################################################################
-# and the time or frequency of the results can be printed with:
-
-print(metadata.time_freq_support)
-
+# Load the dpf.math plugin to have access to fft operators
+base = dpf.BaseService()
+base.load_library("Ans.Dpf.Math.dll","math")
+#you can regenerate the documentation to get the description of math operators
+#doc = dpf.Operator("html_doc")
+#doc.inputs.output_path.connect(r'C:\temp\dpf.html')
+#doc.run()
 
 ###############################################################################
-# Extracting Displacement Results
+# Compute multi harmonic response
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# All results of the model can be accessed through the ``results``
-# property, which returns the ``Results`` class.  This class contains
-# the DPF result operators available to a specific result file, which
-# are listed when printing the object with ``print(results)``
-#
-# Here, we connect the ``'U'`` operator with the data_sources, which
-# takes place automatically when running ``results.displacement()``.
-# By default, this is connected to the first result set, which for this static result is the only result.
-results = model.results
-displacements = results.displacement()
-fields = displacements.outputs.fields_container()
+# In this example we compute the response based on Rz of the selected nodes
+# and through multiple rpms (multiple engine orders)
 
-# finally, extract the data of the displacement field
-disp = fields[0].data
-disp
+# Create a total displacement operator and set its time scoping to
+# the entire time freq support and its nodes scoping into user defined nodes.
+dispOp = model.operator("UTOT")
+timeIds = list(range(1,model.metadata.time_freq_support.n_sets+1))
+
+#define nodal scoping
+nodes = dpf.Scoping()
+nodes.ids = [2,18]
+
+#connect the frequencies and the nodes scopings to the result provider operator
+dispOp.inputs.mesh_scoping.connect(nodes)
+dispOp.inputs.time_scoping.connect(timeIds)
+
+# extract Rz component using the component selector operator
+comp = dpf.Operator("component_selector_fc")
+comp.inputs.connect(dispOp.outputs)
+comp.inputs.component_number.connect(5)
+
+#compute the multi-harmonic response based on Rz and a set of rpms
+rpms = dpf.Scoping()
+rpms.ids = [1,2,3]
+
+fft = dpf.Operator("fft_multi_harmonic_minmax")
+
+fft.inputs.connect(comp.outputs)
+fft.inputs.rpm_scoping.connect(rpms)
+
+
+fields = fft.outputs.field_max()
+len(fields) #we will have a multi-harmonic response per node, here 2 fields
+
+field1 = fields[0]
+field2 = fields[1]
+
+###############################################################################
+# Plot the minimum and maximum displacements over time
+
+pyplot.plot(field1.data,'r',label='field 1')
+pyplot.plot(field2.data,'b',label="field 2")
+pyplot.xlabel("freqs (Hz)")
+pyplot.ylabel("displacement (m)")
+pyplot.legend()
+pyplot.show()
